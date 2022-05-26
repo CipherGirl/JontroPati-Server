d2500acf875d5a007133021cfc3f2b6b8f7a1a6e
Hasna Hena Mow <hasnahenamow@gmail.com>
2022-05-26 08:14:13 +0600

    Users PUT API for AccessToken

--- index.js
+++ index.js
@@ -24,7 +24,31 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
+    await client.connect();
+    const userCollection = client.db('jontropati').collection('users');
+    const productsCollection = client.db('jontropati').collection('products');
 
+    //======
+    // User
+    //======
+
+    app.put('/user/:email', async (req, res) => {
+      console.log('Inside user/email');
+      const email = req.params.email;
+      const user = req.body;
+      const filter = { email: email };
+      const options = { upsert: true };
+      const updateDoc = {
+        $set: user,
+      };
+      const result = await userCollection.updateOne(filter, updateDoc, options);
+      const token = jwt.sign(
+        { email: email },
+        process.env.ACCESS_TOKEN_SECRET,
+        { expiresIn: '1h' }
+      );
+      res.send({ result, token });
+    });
   } finally {
     //await client.close();
   }
@@ -39,5 +63,3 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
   console.log('Listening to port', port);
 });
-
-
